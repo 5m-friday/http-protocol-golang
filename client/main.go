32249e891a798124a7c966ed814bda4f54a6c8cd
steevehook <steevehook@gmail.com>
2020-12-02 17:23:50 +0200

    create http client and fill out std http server

--- client/main.go
+++ client/main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+)
+
 func main() {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal("could not resolve tcp address:", err)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal("could not dial:", err)
+	}
+	defer conn.Close()
+
+	// WRITE TO CONNECTION
+	reqBody := `{"f": "v"}`
+	httpReq := "GET / HTTP/1.1\r\n"
+	httpReq += fmt.Sprintf("Content-Length: %d\r\n", len(reqBody))
+	httpReq += "Host: localhost:8080\r\n"
+	//httpReq += "Connection: close\r\n"
+	httpReq += "\r\n"
+	httpReq += reqBody
+	_, err = conn.Write([]byte(httpReq))
+	if err != nil {
+		log.Fatal("could not write to connection:", err)
+	}
+	// Server does not respond unless the connection
+	// is either closed by the client or by the server
+	// by providing the `Connection: close` header
+	conn.CloseWrite()
+
+	//READ FROM CONNECTION
+	bs, err := ioutil.ReadAll(conn)
+	if err != nil {
+		log.Fatal("could not read from connection:", err)
+	}
+	fmt.Printf("got this response:\n%s", bs)
 }
--- std-http/main.go
+++ std-http/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		res := `{"healthy": true}`+"\n"
+		_, err := w.Write([]byte(res))
+		if err != nil {
+			log.Fatal("could not write the response", err)
+		}
+
+		bs, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal("could not read response body", err)
+		}
+		fmt.Println("response body:", string(bs))
+	})
+	fmt.Println("listening for clients")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("server error", err)
+	}
+}
